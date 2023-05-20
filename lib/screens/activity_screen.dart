7496a81f865fa59667ca13a690b6f570966f5009
FirdausKhamid <firdauskhamid@student.usm.my>
2023-05-20 13:14:38 +0800

    latest stable run

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:kebun_app/bricks/Widgets%20Example/promo_card.dart';
-
 import '../bricks/Widgets Example/horizontal_card.dart';
 
 class Activity extends StatelessWidget {
